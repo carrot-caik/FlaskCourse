55683a6c81d0e264189c430b35d52b2d298749bb
Robert <10harrob@gmail.com>
2022-04-16 16:15:51 +0100

    Complete part 26, but bug with flash message exists.

--- market/routes.py
+++ market/routes.py
@@ -2,7 +2,7 @@ from nis import cat
 from market import app
 from flask import render_template, redirect, url_for, flash, request
 from market.models import Item, User
-from market.forms import RegisterForm, LoginForm, PurchaseItemForm
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm, SellItemForm
 from market import db
 from flask_login import login_user, logout_user, login_required, current_user
 
@@ -15,8 +15,10 @@ def home_page():
 @login_required
 def market_page():
     purchase_form = PurchaseItemForm()
+    selling_form = SellItemForm()
 
     if request.method == 'POST':
+        #Purchase item logic
         purchased_item = request.form.get('purchased_item')
         p_item_object = Item.query.filter_by(name=purchased_item).first()
 
@@ -26,11 +28,26 @@ def market_page():
                 flash(f'Purchased {p_item_object.name} for £{p_item_object.price}.', category='success')
             else:
                 flash(f'Insufficient funds to purchase {p_item_object.name}.', category='danger')
-    
+
+        #Sell item logic
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        
+        if s_item_object:
+            if current_user.can_sell(s_item_object):
+                print("h1")
+                s_item_object.sell(current_user)
+                print("hi")
+                flash(f'Sold {p_item_object.name}.', category='success')
+            else:
+                flash(f'Something went wrong with selling {s_item_object.name}.', category='danger')
+
         return redirect(url_for('market_page'))
     if request.method == "GET":
         items = Item.query.filter_by(owner=None)
-        return render_template('market.html', items=items, purchase_form=purchase_form)
+        owned_items = Item.query.filter_by(owner=current_user.id)
+
+        return render_template('market.html', items=items, purchase_form=purchase_form, owned_items=owned_items, selling_form=selling_form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
